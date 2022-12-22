f29ddd348f72e4f47608a292102edba926824359
Kim Hye Ri <37619441+m3rri@users.noreply.github.com>
2022-12-22 09:33:13 +0900

    Delete counting-dna-nucleotides.js

--- dna/counting-dna-nucleotides.js
+++
@@ -1,5 +0,0 @@
-//https://rosalind.info/problems/dna/
-const nucleotides = ["A", "C", "G", "T"];
-function count(dataset){
-    return nucleotides.map(nucleotide=>dataset.replace(new RegExp(`[^${nucleotide}]`, 'g'), '').length).join(" ");
-}

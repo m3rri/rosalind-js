d93120b50826513a11725b5c92e08b4b3d92cfa2
Kim Hye Ri <37619441+m3rri@users.noreply.github.com>
2022-12-22 09:48:17 +0900

    Create finding-a-motif-in-dna.mjs

---
+++ string-algorithms/finding-a-motif-in-dna.mjs
@@ -0,0 +1,21 @@
+//https://rosalind.info/problems/subs/
+let findMotif = (datasetS, datasetT)=>{
+    const lengthS = datasetS.length;
+    const lengthT = datasetT.length;
+    let collectionOfJ = [];
+
+    for(let i=0; i<lengthS-lengthT; i++){
+        const substringOfS = datasetS.substring(i, i+lengthT);
+        if(substringOfS==datasetT){
+            collectionOfJ.push(i+1);
+        }
+    }
+
+    return collectionOfJ.join(' ');
+}
+
+export default (dataset)=>{
+    const [s, t] = dataset.split('\n');
+    
+    return findMotif(s, t);
+}

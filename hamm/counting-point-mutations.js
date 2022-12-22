489aa938ebedfc6f9b916106f826303f8aef1268
Kim Hye Ri <37619441+m3rri@users.noreply.github.com>
2022-12-22 09:59:20 +0900

    Delete counting-point-mutations.js

--- hamm/counting-point-mutations.js
+++
@@ -1,7 +0,0 @@
-//https://rosalind.info/problems/hamm/
-function countMutation(datasetS, datasetT){
-    return datasetS
-    .split("")
-    .map((dataS, i)=>dataS!=datasetT[i]?1:0)
-    .reduce((acc, curr)=>acc+curr);
-}

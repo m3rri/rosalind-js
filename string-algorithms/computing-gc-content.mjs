2cbcea2de17115ad90de517a0129bdf0a7c156ee
Kim Hye Ri <37619441+m3rri@users.noreply.github.com>
2022-12-23 09:49:02 +0900

    Create computing-gc-content.mjs

---
+++ string-algorithms/computing-gc-content.mjs
@@ -0,0 +1,42 @@
+//https://rosalind.info/problems/gc/
+const makeObject = (name, list)=>{return {seqName: name, sequence: list.join('')}};
+const fastaToSeqListHasSeqName = (dataset)=>{
+    let result = [];
+    let sequenceName = '';
+    let sequenceList = [];
+    
+    dataset
+    .split('\n')
+    .forEach(data=>{
+        if(data.startsWith('>')){
+            sequenceList.length>0 && result.push(makeObject(sequenceName, sequenceList));
+
+            sequenceList = [];
+            sequenceName = data;
+        }else{
+            sequenceList.push(data);
+        }
+    });
+
+    result.push(makeObject(sequenceName, sequenceList));
+
+    return result;
+}
+const computingGCContent = (sequence)=>sequence.replace(/[^GC]/g, '').length/sequence.length*100;
+
+function computingMaxGCContent(dataset){
+    const sequenceList = fastaToSeqListHasSeqName(dataset);
+    const GCList = sequenceList
+        .map(({seqName, sequence})=>{
+            return {
+                seqName: seqName.substring(1),
+                content: computingGCContent(sequence)
+            };
+        })
+        .sort((a, b)=> a.content-b.content);
+    const maxGCItem = GCList.pop();
+    
+    return `${maxGCItem.seqName}\n${maxGCItem.content}`;
+}
+
+export default dataset=>computingMaxGCContent(dataset);
